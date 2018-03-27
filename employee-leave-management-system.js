1cb7ed2e7747d04722409a994b35eb1cff761e03
Rachit Belwariar <gbelwariar@gmail.com>
2018-03-27 13:31:15 +0530

    This commit does the following things -
    1) Configure routes used in the SPA.
    2) Enable HTML5 mode to remove the hashes("#") from the URLs.

--- employee-leave-management-system.js
+++ employee-leave-management-system.js
@@ -1,1 +1,30 @@
-angular.module('employeeLeaveManagementSystem', []);
+angular
+		.module('employeeLeaveManagementSystem', ['ui.router'])
+		.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+			$locationProvider.html5Mode({
+				enabled: true,
+				requireBase: false
+			});
+			$stateProvider
+				.state('home', {
+					url: '/',
+					templateUrl: 'home/home.html',
+					controller: 'HomeCtrl as homeCtrl'
+				})
+				.state('head', {
+					url: '/head',
+					templateUrl: 'head/head.html',
+					controller: 'HeadCtrl as headCtrl',
+					params: {
+						username: 'head'
+					}
+				})
+				.state('employee', {
+					url: '/employee',
+					templateUrl: 'employee/employee.html',
+					controller: 'EmployeeCtrl as employeeCtrl',
+					params: {
+						username: 'employee'
+					}
+				});
+		}]);
