ce6128fc03d17d015e1b5f1165145a8d3b524e94
Rachit Belwariar <gbelwariar@gmail.com>
2018-04-05 09:18:18 +0530

    Log the correct application name when the server is brought up.

--- server.js
+++ server.js
@@ -152,7 +152,7 @@ app.post('/update-leave-request', function(request, response) {
 
 
 var server = app.listen(8080, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
+   var host = server.address().address;
+   var port = server.address().port;
+   console.log("Employee Leave Management Application listening at http://%s:%s", host, port);
 })
